ea38ffaa48936da45af34cee9d845e37d90b83e4
jfperondini <jfperondini@gmail.com>
2022-09-17 14:15:51 -0300

    first

--- drink-eat/js/calculate.js
+++ drink-eat/js/calculate.js
@@ -5,14 +5,17 @@ function calculateEatDrinkBy() {
     princeDrink = document.getElementById("priceFood").value
     personDrink = document.getElementById("food").value
     
-    resultDrink = document.getElementById("resultFood").innerHTML = 
-    parseFloat((princeDrink / personDrink).toFixed(2))
-    
+    resultDrink = document.getElementById("resultFood").innerHTML = parseFloat
+    ((princeDrink / personDrink).toFixed(2))
 
+    if(isNaN(resultDrink)) resultDrink = 0
+   
     priceFood = document.getElementById("priceDrink").value
     personFood = document.getElementById("drink").value
     
-    resultDrink = document.getElementById("resultFoodDrink").innerHTML = parseFloat(((priceFood / personFood) + resultDrink.toFixed(2))
+    resultDrinkFood = document.getElementById("resultFoodDrink").innerHTML = parseFloat
+        (((priceFood / personFood) + resultDrink.toFixed(2)))
 
+    if(isNaN(resultDrinkFood)) resultDrinkFood = 0
 };
 
